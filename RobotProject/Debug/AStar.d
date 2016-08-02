b45abe908e27faeb9c94f64b5447f7479587df31
aivrit <a.ivrit@gmail.com>
2016-08-02 07:57:57 -0700

    hh

---
+++ RobotProject/Debug/AStar.d
@@ -0,0 +1,19 @@
+AStar.d: ../AStar.cpp ../AStar.h ../../Map/Map.h ../../Map/../Consts.h \
+ ../../Map/../MapLoader/lodepng.h ../../Map/../MapLoader/MapInit.h \
+ ../../Consts.h ../Point.h ../Location.h
+
+../AStar.h:
+
+../../Map/Map.h:
+
+../../Map/../Consts.h:
+
+../../Map/../MapLoader/lodepng.h:
+
+../../Map/../MapLoader/MapInit.h:
+
+../../Consts.h:
+
+../Point.h:
+
+../Location.h:
---
+++ RobotProject/Debug/Point.d
@@ -0,0 +1,5 @@
+Point.d: ../Point.cpp ../Point.h ../Location.h
+
+../Point.h:
+
+../Location.h:
